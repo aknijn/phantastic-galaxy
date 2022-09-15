10b7b0a19c3a2f821c0e398b632119181490d6a7
aknijn <arnold.knijn@iss.it>
2022-09-15 14:44:53 +0200

    PHANtG

--- PHANtV/PHANtG.pl
+++ PHANtV/PHANtG.pl
@@ -1,21 +1,22 @@
 #!/usr/bin/env perl
-## A wrapper script to call allele distance matrix and tree
+## A wrapper script to call grapetree tree with metadata
 use strict;
 use warnings;
 use Cwd;
 use English;
 use File::Copy;
 use File::Basename;
+use File::Path;
 use DBI;
 use Config::Simple;
 
 # Parse arguments
 my ($input_files,
     $species,
-    $useNames,
-    $phantv_am,
-    $phantv_dm,
-    $phantv_tree) = @ARGV;
+    $phantg_am,
+    $phantg_metadata,
+    $phantg_tree,
+    $phantg_grapetree) = @ARGV;
 
 # Run program
 my $abs_path = Cwd::abs_path($PROGRAM_NAME);
@@ -25,18 +26,17 @@ my $dsn = $cfg->param('db.dsn');
 my $user = $cfg->param('db.user');
 my $pwd = $cfg->param('db.password');
 if ($species eq "Shiga toxin-producing Escherichia coli") { $species = "Escherichia coli"; };
-runAlleleObserver();
+my $files_id = getIdFilesString($input_files);
+runGrapeTree($files_id);
+createMetadataFile($files_id);
+createGrapeTreeLink();
 exit(0);
 
-# Get allele file with clean columns and calculate the distance matrix and the phylogenetic NJ tree
-sub runAlleleObserver {
-    # get idFiles from filepaths
-    open my $if, '<', $input_files;
-    chomp(my @inputs = <$if>);
-    close $if;
-    my $files_id = getIdFilesString(@inputs);
+# Get allele file with clean columns and calculate the phylogenetic Minimum Spanning Tree using GrapeTree
+sub runGrapeTree {
+    my ($files_id) = @_;
     createAllelesFile($files_id);
-    if ($useNames eq "true") { substituteCodesByNames($files_id); }
+    substituteCodesByNames($files_id);
     # copy filtered allele profiles in allele matrix file (columns with INF, LNF, ecc. are removed)
     my $cmd = q( awk -F"\t" '{(NR>1)}{for(i=2;i<=NF;i++){if ($i ~ /^0$|[a-zA-Z+]+/){print i}}}' cgMLST.tmp | sort | uniq > cgMLST.nocols );
     system($cmd);
@@ -44,16 +44,14 @@ sub runAlleleObserver {
     chomp(my @noCols = <$tf>);
     close $tf;
     my $noColsString = join ',', @noCols;
-	if ($noColsString eq "") {
-        copy("cgMLST.tmp",$phantv_am);
+    if ($noColsString eq "") {
+        copy("cgMLST.tmp",$phantg_am);
     } else {
-        my $cmd2 = "cut --complement -f" . $noColsString . " cgMLST.tmp > " . $phantv_am;
+        my $cmd2 = "cut --complement -f" . $noColsString . " cgMLST.tmp > " . $phantg_am;
         system($cmd2);
     }
-    # calculate distance matrix from allele profiles
-    my $result = system("python $scriptdir/scripts/mlst_hash_stretch_distance.py -i cgMLST.tmp -o $phantv_dm");
-    # calculate tree from distance matrix
-    system("$scriptdir/scripts/mentalist_tree $phantv_dm > $phantv_tree");
+    # calculate tree from allele profiles
+    my $result = system("python grapetree.py -p $phantg_am -m MSTreeV2 > $phantg_tree");
     return 0;
 }
 
@@ -64,9 +62,12 @@ sub getIdFile {
     return $dirs[5];
 }
 
-# Obtain a string with the idFiles
+# Obtain a string with the idFiles from filepaths
 sub getIdFilesString {
-    my @inFiles = @_;
+    my ($inputFiles) = @_;
+    open my $if, '<', $inputFiles;
+    chomp(my @inFiles = <$if>);
+    close $if;
     my @inIdFiles;
     foreach (@inFiles) {
         push @inIdFiles, getIdFile($_);
@@ -152,4 +153,58 @@ sub substituteCodesByNames {
     system($cmd);
     move("cgMLST.tmp","cgMLST.tmp.OLD");
     move("cgMLST.tmp2","cgMLST.tmp");
+}
+
+# Obtain metadata from db and write to output file
+sub createMetadataFile {
+    my ($files_id) = @_;
+    my $sql;
+
+    if ($species eq "Escherichia coli") {
+        $sql = "select * from v_grapetree_listeria where files_id IN ($files_id)";
+    } else {
+        if ($species eq "Listeria monocytogenes") {
+            $sql = "select * from v_grapetree_listeria where files_id IN ($files_id)";
+        } else { $sql = "select * from v_grapetree_listeria where files_id = -1"; }
+    }
+
+    # connect to MySQL database
+    my %attr = ( PrintError=>0, RaiseError=>1);
+    my $dbh = DBI->connect($dsn,$user,$pwd,\%attr);
+    my $sth = $dbh->prepare($sql);
+    $sth->execute();
+    open my $if, '>', "$phantg_metadata" or die "Cannot open $phantg_metadata: $!";
+    if ($species eq "Escherichia coli") { print $if "Campione\tRegione\tAnno\tAntigen_O\tAntigen_H\tCluster_id\teae\tehxA\tMLST_ST\tQC\tstx1\tstx2\tstx_subtype\n"; } 
+    else { if ($species eq "Listeria monocytogenes") { print $if "Campione\tRegione\tAmplicons\tAnno\tCluster_id\tMLST_CC\tMLST_Lineage\tQC\tSeroGroup\tSeroType\n"; } }
+    while (my @row = $sth->fetchrow_array) { 
+      if ($species eq "Escherichia coli") { print $if "$row[1]\t$row[10]\t$row[2]\t$row[3]\t$row[4]\t$row[5]\t$row[6]\t$row[7]\t$row[8]\t$row[9]\t$row[11]\t$row[12]\t$row[13]\n"; } 
+      else { if ($species eq "Listeria monocytogenes") { print $if "$row[1]\t$row[8]\t$row[2]\t$row[3]\t$row[4]\t$row[5]\t$row[6]\t$row[7]\t$row[9]\t$row[10]\n"; } }
+    }       
+    close $if;
+
+    $sth->finish();
+    # disconnect from the MySQL database
+    $dbh->disconnect();
+}
+
+sub createGrapeTreeLink {
+    # copy $phantg_metadata & $phantg_tree to a shared path visible by GrapeTree on the server vizapp.iss.it
+    my $grape_path = "/gfs/vizapp/grapetree/";
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+	$year = 1900 + $year;
+    my $stamp = sprintf("%04d%02d%02d%02d%02d%02d", $year, $mon+1, $mday, $hour, $min, $sec);
+    my $grape_path_year = $grape_path + $year;
+	# create the path if it doesn't exist yet
+	if ( !-d $grape_path_year) { make_path $grape_path_year or die "Failed to create path: $grape_path_year"; }
+	# create unique filenames
+    my $grape_metadata = $year + "/ISS_" + $stamp + ".tsv";
+	my $grape_tree = $year + "/ISS_" + $stamp + ".nwk";
+    copy($phantg_metadata,$grape_path + $grape_metadata);
+    copy($phantg_tree,$grape_path + $grape_tree);
+    # create the html file linking the tree and metadata files
+    open my $of, '>', "$phantg_grapetree" or die "Cannot open $phantg_grapetree: $!";
+    print $of "<!DOCTYPE html><html><body>\n";
+    print $of "<strong><a href=\"https:\/\/irida.iss.it\/grapetree\/?tree=$grape_tree\&metadata=$grape_metadata\">visualizza albero filogenetico<\/a><\/strong>\n";
+    print $of "<\/body><\/html>\n";
+    close $of;
 }
