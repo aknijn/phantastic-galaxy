c88665043234e970e962e6b82b1dec43b63f626d
aknijn <arnold.knijn@iss.it>
2022-12-16 13:04:48 +0100

    Update PHANtR.py

--- PHANtV/PHANtR.py
+++ PHANtV/PHANtR.py
@@ -140,9 +140,8 @@ def writePdf(dataSommario, dataSommarioHeader, dataSommarioHeaderFormat, dataAMR
         pdf.multi_cell(col_width, line_height, dataSommario[i], border=1, new_x="RIGHT", new_y="TOP", align="CENTER", fill=True)
     pdf.ln(line_height)
     pdf.set_font("helvetica", "", 10)
-    if dataSommarioHeader[8] == "stx_subtype":
-        if "(*)" in dataSommario[8]:
-            pdf.write(8, "(*)=subtype con identità >95% e <100%")
+    if len(dataSommarioHeader) > 8 and dataSommarioHeader[8] == "stx_subtype" and "(*)" in dataSommario[8]:
+        pdf.write(8, "(*)=subtype con identità >95% e <100%")
 
     pdf.ln(20)
     pdf.set_font("helvetica", "BU", 14)
