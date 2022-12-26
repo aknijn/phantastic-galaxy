5f9a6fc8aeb7ba84091dc08d057da803f7fe7b74
aknijn <arnold.knijn@iss.it>
2022-12-26 15:37:37 +0100

    Update run_rematch.py

--- PHANtT-Lm/scripts/modules/run_rematch.py
+++ PHANtT-Lm/scripts/modules/run_rematch.py
@@ -68,8 +68,8 @@ def determine_general_statistics(outdir, sample_data, minimum_gene_coverage, min
     mean_sample_coverage = 0
     with open(os.path.join(outdir, 'rematchModule_report.txt'), 'wt') as writer:
         #writer.write('\t'.join(['#gene', 'percentage_gene_coverage', 'gene_mean_read_coverage', 'percentage_gene_low_coverage', 'number_positions_multiple_alleles', 'percentage_gene_identity']) + '\n')
-        writer.write('\t'.join([sample_data['header'], str(round(sample_data['gene_coverage'], 2)), str(round(sample_data['gene_mean_read_coverage'], 2)), str(round(sample_data['gene_low_coverage'], 2)), str(sample_data['gene_number_positions_multiple_alleles']), str(round(sample_data['gene_identity'], 2))]) + '\n')
         for i in range(1, len(sample_data) + 1):
+            writer.write(sample_data[i]) + '\n')
             #writer.write('\t'.join([sample_data[i]['header'], str(round(sample_data[i]['gene_coverage'], 2)), str(round(sample_data[i]['gene_mean_read_coverage'], 2)), str(round(sample_data[i]['gene_low_coverage'], 2)), str(sample_data[i]['gene_number_positions_multiple_alleles']), str(round(sample_data[i]['gene_identity'], 2))]) + '\n')
             if sample_data[i]['gene_coverage'] < minimum_gene_coverage or \
                     sample_data[i]['gene_identity'] < minimum_gene_identity:
