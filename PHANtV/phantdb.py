66f0c65bc29cb2302de8322e955de3a9ca7d6ec6
aknijn <arnold.knijn@iss.it>
2023-02-21 15:06:47 +0100

    Update phantdb.py

--- PHANtV/phantdb.py
+++ PHANtV/phantdb.py
@@ -146,10 +146,9 @@ class IridaDb:
         return str(row[0])
         
     def user_in_role(self, username, userrole):
-        sql = "SELECT email,name from user_group_member \
+        sql = 'SELECT email,name from user_group_member \
           INNER JOIN user on(user.id=user_group_member.user_id) \
-          INNER JOIN user_group on(user_group.id=user_group_member.group_id) WHERE email='%s' and name='%s'"
-        print(sql % (username, userrole))
+          INNER JOIN user_group on(user_group.id=user_group_member.group_id) WHERE email="%s" and name="%s"'
         number_of_rows = self.execute(sql, (username, userrole))
         return number_of_rows is not None  
 
