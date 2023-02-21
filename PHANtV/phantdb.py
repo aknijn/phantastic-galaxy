0a7e633ad73620475bbbf63cae4b8e6e6ef9d6df
aknijn <arnold.knijn@iss.it>
2023-02-21 15:13:41 +0100

    Update phantdb.py

--- PHANtV/phantdb.py
+++ PHANtV/phantdb.py
@@ -146,12 +146,12 @@ class IridaDb:
         return str(row[0])
         
     def user_in_role(self, username, userrole):
-        sql = 'SELECT email,name from user_group_member \
+        sql = 'SELECT COUNT(*) from user_group_member \
           INNER JOIN user on(user.id=user_group_member.user_id) \
           INNER JOIN user_group on(user_group.id=user_group_member.group_id) WHERE email="%s" and name="%s"'
-        print(sql % (username, userrole))
-        number_of_rows = self.execute(sql, (username, userrole))
-        return number_of_rows is not None  
+        self.execute(sql, (username, userrole))
+        row = self.fetchone()
+        return row[0] > 0
 
 class StecDb:
     def __init__(self, species):
