3c93a3b0059ab88cdb054892ca28cea5e912a20c
aknijn <arnold.knijn@iss.it>
2023-02-21 14:40:03 +0100

    Update phantdb.py

--- PHANtV/phantdb.py
+++ PHANtV/phantdb.py
@@ -115,8 +115,8 @@ class IridaDb:
               'phantastic_aq' AND aofm_seq.analysis_output_file_key = 'seqtype' AND aofm_vir.analysis_output_file_key = 'virulotypes' AND \
               sfpf.files_id IN (?) group by sample.id"
         else:
-            sql = "SELECT * FROM sample LIMIT 0"
-        return self.query(sql, (self.sequence_path, self.sequence_path, self.output_path, self.output_path, self.output_path, self.output_path, fileIds,))
+            sql = "SELECT * FROM sample WHERE sampleName=? AND sampleName=? AND sampleName=? AND sampleName=? AND sampleName=? AND sampleName=? AND id=? LIMIT 0"
+        return self.query(sql, (self.sequence_path, self.sequence_path, self.output_path, self.output_path, self.output_path, self.output_path, fileIds))
 
     def allele_strain(self, sampleCode):
         if self.species == 'Shiga toxin-producing Escherichia coli':
@@ -124,15 +124,15 @@ class IridaDb:
         elif self.species == "Listeria monocytogenes":
             sql = "SELECT allele_strain FROM mlst_listeria WHERE sample_code=?"
         else:
-            sql = "SELECT * FROM allele_strain LIMIT 0"
-        return self.query(sql, (sampleCode,))
+            sql = "SELECT * FROM allele_strain WHERE sample_code=? LIMIT 0"
+        return self.query(sql, (sampleCode))
 
     def update_externalId(self, analyticalPipelineRunId, sampleId):
         if inspecies == 'Shiga toxin-producing Escherichia coli':
             sql = "UPDATE sample SET externalId=? WHERE id=?"
         else:
-            sql = "SELECT * FROM sample LIMIT 0"
-        self.execute(sql, (analyticalPipelineRunId, sampleId,))
+            sql = "SELECT * FROM sample WHERE externalId=? AND id=? LIMIT 0"
+        self.execute(sql, (analyticalPipelineRunId, sampleId))
  
     def get_singleend_coverage(self, file_id):
         sql = "SELECT total_bases/genome_size FROM sequence_file_pair_files AS sfpf \
@@ -141,7 +141,7 @@ class IridaDb:
           INNER JOIN project_sample AS ps on sso.sample_id = ps.sample_id \
           INNER JOIN project AS p on ps.project_id = p.id \
           WHERE genome_size>0 AND sfpf.files_id = ?"
-        self.execute(sql, (file_id,))
+        self.execute(sql, (file_id))
         row = self.fetchone()
         return str(row[0])
         
@@ -149,7 +149,7 @@ class IridaDb:
         sql = "select email,name from user_group_member \
           INNER JOIN user on(user.id=user_group_member.user_id) \
           INNER JOIN user_group on(user_group.id=user_group_member.group_id) WHERE email=? and name=?"
-        number_of_rows = self.execute(sql, (username, userrole,))
+        number_of_rows = self.execute(sql, (username, userrole))
         return number_of_rows > 0   
 
 class StecDb:
@@ -215,7 +215,7 @@ class StecDb:
         if self.species == 'Shiga toxin-producing Escherichia coli':
             #sample_name, sampId, sampPoint, sampCountry, origCountry, sampArea, sampY, sampM, sampD, sampling_matrix_code, sampling_matrix_free_text, isolId, YEAR(DateOfSampling), MONTH(DateOfSampling), DAY(DateOfSampling)
             sql = "SELECT ISS_ID, sampId, sampPoint, sampCountry, origCountry, sampArea, sampY, sampM, sampD, sampMatCode, '', isolId, \
-              YEAR(DateOfSampling), MONTH(DateOfSampling), DAY(DateOfSampling) FROM Samples WHERE ISS_ID='?'"
+              YEAR(DateOfSampling), MONTH(DateOfSampling), DAY(DateOfSampling) FROM Samples WHERE ISS_ID=?"
         else:
-            sql = "SELECT * FROM Samples TOP 0"
+            sql = "SELECT * FROM Samples WHERE ISS_ID=? TOP 0"
         return self.query(sql, (sample_name,))
