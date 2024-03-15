bd02a66155e16aba3b5a348fe0bcf3262a2e06b0
aknijn <arnold.knijn@iss.it>
2024-03-15 15:53:55 +0100

    sample_id

--- PHANtC-Tree/PHANtC-TreeEc.pl
+++ PHANtC-Tree/PHANtC-TreeEc.pl
@@ -25,7 +25,7 @@ my $cfg = new Config::Simple("$scriptdir/../phantastic.conf");
 my $dsn = $cfg->param('db.dsn');
 my $user = $cfg->param('db.user');
 my $pwd = $cfg->param('db.password');
-my $sample_id = substr($sample_code,2);
+my $sample_id = split('_*', $sample_code)[1];
 my ($antigen_o, $sample_metadata) = readJsonFile();
 if ($sample_metadata eq "ND") {
     open FILEHANDLE, '>', $phantcec_tree and close FILEHANDLE or die "Failed to create file: $!\n";
