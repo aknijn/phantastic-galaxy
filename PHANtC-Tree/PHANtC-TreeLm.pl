e19d1c91c60a978f1f9b6f09b856c95ec6b8315f
aknijn <arnold.knijn@iss.it>
2024-05-04 20:19:39 +0200

    serogroup

--- PHANtC-Tree/PHANtC-TreeLm.pl
+++ PHANtC-Tree/PHANtC-TreeLm.pl
@@ -31,7 +31,7 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 $year = 1900 + $year;
 my $stamp = sprintf("%04d%02d%02d%02d%02d%02d", $year, $mon+1, $mday, $hour, $min, $sec);
 my $outputname = "ISS" . $stamp;
-my $sample_metadata = readJsonFile();
+my ($serogroup, $sample_metadata) = readJsonFile();
 if ($sample_metadata eq "ND") {
     open FILEHANDLE, '>', $phantclm_tree and close FILEHANDLE or die "Failed to create file: $!\n";
     open FILEHANDLE, '>', $phantclm_dm and close FILEHANDLE or die "Failed to create my : $!\n";
@@ -79,6 +79,7 @@ sub readJsonFile {
     read $jf, my $json_text, -s $jf;
     chomp($json_text);
     my $metadata = "ND";
+    my $serogroup = "ND";
     my $json_var = decode_json substr($json_text,1,-1);
     my $contaminated = $json_var->{qc_messages};
     if (index($contaminated, "contaminated") == -1) {
@@ -86,7 +87,7 @@ sub readJsonFile {
         my $region = $json_var->{region};
         my $country = "Italy";
         my $date_campione_condizioneclinica_origine = getSampleMetadata();
-        my $serogroup = $json_var->{serotype_serogroup};
+        $serogroup = $json_var->{serotype_serogroup};
         my $amplicons = $json_var->{serotype_amplicons};
         my $mlst_st = $json_var->{mlst_ST};
         my $mlst_cc = $json_var->{mlst_CC};
@@ -94,7 +95,7 @@ sub readJsonFile {
 		my ($latitude, $longitude) = getLatLong($region);
         $metadata = join("\t", $sequence, $region, $country, $date_campione_condizioneclinica_origine, $serogroup, $amplicons, $mlst_st, $mlst_cc, $mlst_lineage, $latitude, $longitude)
     }
-    return $metadata;
+    return ($serogroup, $metadata);
 }
 
 # Obtain metadata from db not present in json file
@@ -118,7 +119,8 @@ sub getSampleMetadata {
 # Obtain allele profiles from db with allele_coverage>79,9% and write to temp file
 sub createAllelesFile {
     my $sql = "select allele_strain from mlst_listeria where sample_code='FILE' union
-               select allele_strain from mlst_listeria where permille_loci>799";
+               select allele_strain from mlst_listeria where sample_code='$sample_code' union
+               select allele_strain from v_mlst_listeria where permille_loci>799 and Serogroup = '$serogroup'";
     # connect to MySQL database
     my %attr2 = ( PrintError=>0, RaiseError=>1);
     my $dbh2 = DBI->connect($dsn,$user,$pwd,\%attr2);
@@ -139,7 +141,7 @@ sub createAllelesFile {
 
 # Obtain metadata from db and write to output file
 sub createMetadataFile {
-    my $sql = "select Ceppo,Regione,InizioSintomi,CondizioneClinica,Origine,Sequence,Serogroup,Amplicons,MLST_ST,MLST_CC,MLST_Lineage,latitude,longitude from v_listeria_opendata";
+    my $sql = "select Ceppo,Regione,InizioSintomi,CondizioneClinica,Origine,Sequence,Serogroup,Amplicons,MLST_ST,MLST_CC,MLST_Lineage,latitude,longitude from v_listeria_opendata where Serogroup ='$serogroup'";
     # connect to MySQL database
     my %attr = ( PrintError=>0, RaiseError=>1);
     my $dbh = DBI->connect($dsn,$user,$pwd,\%attr);
