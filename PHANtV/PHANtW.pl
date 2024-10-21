052bf7f412c4d2486b82f78433dd1a2df94853fd
aknijn <arnold.knijn@iss.it>
2024-10-21 13:33:14 +0200

    poppunk

--- PHANtV/PHANtW.pl
+++ PHANtV/PHANtW.pl
@@ -18,18 +18,20 @@ my $scriptdir = dirname($abs_path);
 prepareEnvironment();
 my $result=runPopPUNK();
 #move("output/output_core_NJ.nwk", $phantw_tree);
-system(qq(sed 's-\\x27--g' output/output_core_NJ.nwk > $phantw_tree));
+system(qq(sed 's-\\x27--g' output_viz/output_viz_core_NJ.nwk > $phantw_tree));
 system("sed 's-input_dir/--g' distances.txt > $phantw_cl");
 
 exit($result);
 
 # Run PopPUNK
 sub runPopPUNK {
-    my $result_db = system("poppunk --create-db --r-files inputfiles.txt --threads 4 --k-step 2 --min-k 9 --plot-fit 0 --overwrite --output dbdir");
+    my $result_db = system("poppunk --create-db --r-files $input_file --threads 4 --k-step 2 --min-k 9 --plot-fit 0 --overwrite --output dbdir");
     my $result_fit = system("poppunk --fit-model dbscan --threads 4 --output output --full-db --K 2 --microreact --ref-db dbdir --distances dbdir/dbdir.dists");
+	system("poppunk_visualise --ref-db dbdir --model-dir output --output output_viz --microreact");
     system("python $scriptdir/scripts/extract_distances.py  --distances dbdir/dbdir.dists --output distances.txt");
     return $result_db+$result_fit;
 }
+
 
 # Run prepareEnvironment, create the directory $indir with symlinks to the files listed in $input_file
 sub prepareEnvironment {
