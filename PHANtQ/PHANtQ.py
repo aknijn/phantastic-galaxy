a78deb3ea3b9dae143b46fd258c1b49d82c29927
Arnold Knijn <arnold.knijn@iss.it>
2025-03-20 15:02:33 +0100

    Query_Coverage

--- PHANtQ/PHANtQ.py
+++ PHANtQ/PHANtQ.py
@@ -33,7 +33,7 @@ def __main__():
         table_data = [[str(col).rstrip() for col in row.split('\t')] for row in table_in]
     if len(table_data[0]) == 19:
         for row in table_data:
-            if row[18] != args.species and row[18] != 'Species' and float(row[6]) > 10.0:
+            if row[18] != args.species and row[18] != 'Species' and float(row[5]) > 10.0:
                 if strContamination != 'No':
                     strContamination = strContamination + ", " + row[18]
                 else:
